97535478443ddb9c2e836dee6ab4a6536be05be0
Felipe Seabra <feliperoce@gmail.com>
2024-02-29 12:35:56 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './global.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
