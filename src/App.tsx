fde1bb87d636d0a6a127f357c3e25b90df87a072
Felipe Seabra <feliperoce@gmail.com>
2024-02-29 12:39:43 -0300

    feat: test tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1 className="font-bold">Home</h1>} />
         <Route path="*" element={<h1>404 - Not Found</h1>} />
       </Routes>
     </Router>
